c758de594bddf838e97875ca1b578143f5a10964
Тимур Бадалбаев <timurbadalbaev19@gmail.com>
2022-11-13 17:56:59 +0300

    add program

---
+++ program/Program.cs
@@ -0,0 +1,31 @@
+﻿// 
+
+using System;
+using static System.Console;
+
+Clear();
+WriteLine("Введите элементы массива через пробел");
+string[] arr = ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+int num = 3;
+
+string[] newArr = GetSelection(arr, num);
+
+string[] GetSelection(string[] arr, int n) {
+    string[] newArr = new string [arr.Length];
+    int count = -1;
+    for(int i = 0; i < arr.Length; i++) {
+        if(arr[i].Length <= 3) {
+            count++;
+            newArr[count] = arr[i];
+        }
+    }
+    
+    string[] result = new string [count + 1];
+    for(int i = 0; i <= count; i++) {
+        result[i] = newArr[i];
+    }
+
+    return result;
+}
+
+WriteLine(String.Join(", ", newArr));
